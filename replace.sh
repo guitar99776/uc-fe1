c0717864227c306e6d9d4bc0c5581bb95009991c
linyunhe:linyunhe <your.email@example.com>
2023-08-14 20:48:41 +0800

    1

--- replace.sh
+++ replace.sh
@@ -2,8 +2,8 @@
 
 echo $TEST_SELECT $BUILDING_IMMEDIATELY $TEST_INPUT
 
-sed -i 's#'''TEST_SELECT'''#'''$TEST_SELECT'''#g' /test.json
-sed -i 's/BUILDING_IMMEDIATELY/$BUILDING_IMMEDIATELY/' /test.json
-sed -i 's/TEST_INPUT/$TEST_INPUT/' /test.json
+sed -i 's#'''TEST_SELECT'''#'''$TEST_SELECT'''#g' test
+sed -i 's/BUILDING_IMMEDIATELY/$BUILDING_IMMEDIATELY/' test.json
+sed -i 's/TEST_INPUT/$TEST_INPUT/' test.json
 
 cat test.json
