7905013d70e2ed411abecee7553bc861a8345059
linyunhe:linyunhe <your.email@example.com>
2023-08-14 20:44:10 +0800

    测试

--- replace.sh
+++ replace.sh
@@ -2,7 +2,7 @@
 
 echo $TEST_SELECT $BUILDING_IMMEDIATELY $TEST_INPUT
 
-sed -i 's/TEST_SELECT/$TEST_SELECT/' test.json
+sed -i 's#'''TEST_SELECT'''#'''$TEST_SELECT'''#g' test.json
 sed -i 's/BUILDING_IMMEDIATELY/$BUILDING_IMMEDIATELY/' test.json
 sed -i 's/TEST_INPUT/$TEST_INPUT/' test.json
 
