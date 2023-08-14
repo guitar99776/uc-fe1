ab2340561b15d4b199679ea0570923b054e3c7bb
linyunhe:linyunhe <your.email@example.com>
2023-08-14 21:22:10 +0800

    shell 语句变量替换

--- replace.sh
+++ replace.sh
@@ -2,8 +2,8 @@
 
 echo $TEST_SELECT $BUILDING_IMMEDIATELY $TEST_INPUT
 
-sed -i '.bak' 's/TEST_SELECT/$TEST_SELECT/'   test.json
-sed -i '.bak' 's/BUILDING_IMMEDIATELY/$BUILDING_IMMEDIATELY/'  test.json
-sed -i '.bak' 's/TEST_INPUT/$TEST_INPUT/'  test.json
+sed -i '.bak' "s/@TEST_SELECT/$TEST_SELECT/"   test.json
+sed -i '.bak' "s/@BUILDING_IMMEDIATELY/$BUILDING_IMMEDIATELY/"  test.json
+sed -i '.bak' "s/@TEST_INPUT/$TEST_INPUT/"  test.json
 
 cat test.json
